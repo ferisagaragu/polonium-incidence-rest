a71fcd3ea9e5bb0abd91fdd152cd0fe22fc91026
Fernando Isaías García Aguirre <fernaisagaragu@gmail.com>
2022-03-01 11:55:43 -0600

    Initial commit

---
+++ src/main/kotlin/org/pecheblenda/poloniumincidencerest/PoloniumIncidenceRestApplication.kt
@@ -0,0 +1,11 @@
+package org.pecheblenda.poloniumincidencerest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PoloniumIncidenceRestApplication
+
+fun main(args: Array<String>) {
+	runApplication<PoloniumIncidenceRestApplication>(*args)
+}
---
+++ src/test/kotlin/org/pecheblenda/poloniumincidencerest/PoloniumIncidenceRestApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.pecheblenda.poloniumincidencerest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PoloniumIncidenceRestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
